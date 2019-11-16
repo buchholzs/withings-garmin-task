909dd4ca88758b339b16bfffb74ab6abeed66052
Steffen Buchholz <steffen.buchholz@gmx.de>
2019-11-16 10:19:45 +0100

    Initial version

--- sync-garmin.bat
+++ sync-garmin.bat
@@ -1,8 +1,9 @@
-echo ---------- Starte Sync ----------  >> sync.log
-echo Aktuelles Datum: %date% %time:~0,5% >> sync.log
-set "G=%temp%\LastMonth.vbs"
+echo ---------- Starting Sync ----------  >> sync.log
+echo Current date: %date% %time:~0,5% >> sync.log
+call setenv.bat
+set "G=%temp%\LastPeriod.vbs"
 >%G% echo WScript.Echo DateAdd("d",-30, Date)
-for /f %%i in ('cscript //nologo %G%') do set "LastMonth=%%i"
-echo Hole Daten ab %LastMonth:~-10,2%.%LastMonth:~-7,2%.%LastMonth:~-4,4% >> sync.log
-sync.py -f %LastMonth:~-4,4%-%LastMonth:~-7,2%-%LastMonth:~-10,2% -v --garmin-username=steffen.buchholz@gmx.de --garmin-password=Garmincom$2 >> sync.log
+for /f %%i in ('cscript //nologo %G%') do set "LastPeriod=%%i"
+echo Fetching data from %LastPeriod:~-10,2%.%LastPeriod:~-7,2%.%LastPeriod:~-4,4% >> sync.log
+sync.py -f %LastPeriod:~-4,4%-%LastPeriod:~-7,2%-%LastPeriod:~-10,2% -v --garmin-username=%USERNAME% --garmin-password=%PASSWORD% >> sync.log
 echo ---------- Sync beendet ---------  >> sync.log
