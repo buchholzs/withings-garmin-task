c3cd89bcbd1aa3c13d53302cc32459f1bab33ed7
Steffen Buchholz <steffen.buchholz@gmx.de>
2019-11-16 12:17:06 +0100

    Added Template for Script

--- sync-garmin.bat
+++ sync-garmin.bat
@@ -1,9 +1,12 @@
-echo ---------- Starting Sync ----------  >> sync.log
-echo Current date: %date% %time:~0,5% >> sync.log
+echo ---------- Starting Sync ----------  >> %LOGFILE%
 call setenv.bat
+echo Current date: %date% %time:~0,5% >> %LOGFILE%
+set CURDIR=%cd%
 set "G=%temp%\LastPeriod.vbs"
->%G% echo WScript.Echo DateAdd("d",-30, Date)
+>%G% echo WScript.Echo DateAdd("d",-%DAYS%, Date)
 for /f %%i in ('cscript //nologo %G%') do set "LastPeriod=%%i"
-echo Fetching data from %LastPeriod:~-10,2%.%LastPeriod:~-7,2%.%LastPeriod:~-4,4% >> sync.log
-sync.py -f %LastPeriod:~-4,4%-%LastPeriod:~-7,2%-%LastPeriod:~-10,2% -v --garmin-username=%USERNAME% --garmin-password=%PASSWORD% >> sync.log
-echo ---------- Sync end ---------  >> sync.log
+echo Fetching data from %LastPeriod:~-10,2%.%LastPeriod:~-7,2%.%LastPeriod:~-4,4% >> %LOGFILE%
+cd %SYNCPY%
+sync.py -f %LastPeriod:~-4,4%-%LastPeriod:~-7,2%-%LastPeriod:~-10,2% -v --garmin-username=%USERNAME% --garmin-password=%PASSWORD% >> %LOGFILE%
+cd %CURDIR%
+echo ---------- Sync end ---------  >> %LOGFILE%
