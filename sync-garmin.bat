21ae9cf819a176be2ab0193e773d7f3c4f3d6e6f
Steffen Buchholz <steffen.buchholz@gmx.de>
2024-08-11 23:49:08 +0200

    typo

--- sync-garmin.bat
+++ sync-garmin.bat
@@ -6,5 +6,5 @@ set CURDIR=%cd%
 set "G=LastPeriod.vbs"
 for /f %%i in ('cscript //nologo %G% %DAYS%') do set "LastPeriod=%%i"
 echo Fetching data from %LastPeriod:~-10,2%.%LastPeriod:~-7,2%.%LastPeriod:~-4,4% >> %LOGFILE%
-withings-Sync -f %LastPeriod:~-4,4%-%LastPeriod:~-7,2%-%LastPeriod:~-10,2% -v --garmin-username=%USERNAME% --garmin-password=%PASSWORD% >> %LOGFILE%
+withings-sync -f %LastPeriod:~-4,4%-%LastPeriod:~-7,2%-%LastPeriod:~-10,2% -v --garmin-username=%USERNAME% --garmin-password=%PASSWORD% >> %LOGFILE%
 echo ---------- Sync end ---------  >> %LOGFILE%
